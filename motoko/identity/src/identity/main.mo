fde4d0488c27963492a09e8a3851fccc2aa382c4
John Krüger <johnkruger37@pm.me>
2021-06-06 14:31:17 +0200

    identity WIP - does not quite work, due to 'Invalid combined threshold signature'

--- motoko/identity/src/identity/main.mo
+++ motoko/identity/src/identity/main.mo
@@ -1,5 +1,10 @@
+// see https://sdk.dfinity.org/docs/base-libraries/principal
+import Principal "mo:base/Principal";
+import Debug "mo:base/Debug";
+
 actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+    public /*query ({caller}) */ func greet() : async Text {
+        Debug.print("hey");
+        return "Hello, " // # Principal.toText(caller)
     };
 };
--- motoko/identity/src/identity_assets/src/index.js
+++ motoko/identity/src/identity_assets/src/index.js
@@ -1,12 +1,45 @@
 import { Actor, HttpAgent } from '@dfinity/agent';
-import { idlFactory as identity_idl, canisterId as identity_id } from 'dfx-generated/identity';
+import { idlFactory as identity_example_idl, canisterId as identity_example_canister_id } from 'dfx-generated/identity';
+import { AuthClient } from "@dfinity/auth-client";
 
-const agent = new HttpAgent();
-const identity = Actor.createActor(identity_idl, { agent, canisterId: identity_id });
+const init = async () => {
+  const authClient = await AuthClient.create();
 
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  const greeting = await identity.greet(name);
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  }
 
+  document.getElementById("clickMeBtn").addEventListener("click", async () => {
+    handleLoginLogout(authClient);
+  });
+};
+
+async function handleLoginLogout(authClient) {
+  if (await authClient.isAuthenticated()) {
+    await authClient.logout();
+    document.getElementById("clickMeBtn").innerText = "Login!"
+  } else {
+    await authClient.login({
+        onSuccess: async () => {
+          handleAuthenticated(authClient);
+        },
+    });
+  };
+}
+
+async function handleAuthenticated(authClient) {
+  document.getElementById("clickMeBtn").innerText = "Logout"
+
+  const identity = await authClient.getIdentity();
+  const agent = new HttpAgent({ identity });
+  const identity_example = Actor.createActor(identity_example_idl, {
+    agent,
+    canisterId: identity_example_canister_id,
+  });
+
+  const greeting = await identity_example.greet();
   document.getElementById("greeting").innerText = greeting;
-});
+  // document.getElementById("greeting").innerText = "OK";
+}
+
+init();
